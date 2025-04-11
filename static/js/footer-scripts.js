c579ec9839363de2276d9c9e7fd9d8f33cb94e22
C Tu <snca0606@gmail.com>
2025-04-11 16:26:49 +0630

    Modified the footer text to only translate in x when the viewpoint almost reaches to the end of a page.

--- static/js/footer-scripts.js
+++ static/js/footer-scripts.js
@@ -1,7 +1,9 @@
 window.onscroll = function() {
+  if (window.scrollY + window.innerHeight >= document.documentElement.offsetHeight - 100) {
   var title = this.document.getElementById("footer-main-text");
-  var scrollTop = document.documentElement.scrollTop * 0.5;
+  var scrollTop = document.documentElement.scrollTop * 0.7;
   title.style.transform =
     "translateX(-" + scrollTop + "px)";
+};
 };
 document.getElementById("footer-copyright").innerHTML = new Date().getFullYear();
