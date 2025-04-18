a33ac41f136f6d52499b06f2982e5ca0ab2b484c
C Tu <snca0606@gmail.com>
2025-04-18 22:17:47 +0630

    Made some minor changes in class names, indentations, etc

--- static/js/form_system.js
+++ static/js/form_system.js
@@ -6,32 +6,32 @@ var checkbox = document.getElementById("anonymous");
             contributor_name.value = '';
             contributor_name.placeholder = 'No input required'
             contributor_name.disabled = true;
-
+            /////////////////////////////////
             contributor_email.value = '';
             contributor_email.placeholder = 'No input required'
             contributor_email.disabled = true;
          } else {
             contributor_name.disabled = false;
             contributor_name.placeholder = 'Your name'
-
+            /////////////////////////////////
             contributor_email.disabled = false;
             contributor_email.placeholder = 'Your email'
         }
     });
 
 var full_address = document.getElementById("full-address");
-    full_address.addEventListener('change', function(e) {
-        let address_input = document.getElementById("address");
-         if (full_address.checked) {
-            address_input.type = 'text';
-            address_input.value = '';
-            address_input.placeholder = 'Cafe Address';
-         } else {
-            address_input.type = 'url';
-            address_input.value = '';
-            address_input.placeholder = 'Google Maps Link';
-        }
-    });
+full_address.addEventListener('change', function(e) {
+    let address_input = document.getElementById("address");
+     if (full_address.checked) {
+        address_input.type = 'text';
+        address_input.value = '';
+        address_input.placeholder = 'Cafe Address';
+     } else {
+        address_input.type = 'url';
+        address_input.value = '';
+        address_input.placeholder = 'Google Maps Link';
+    }
+});
 
 var title = document.getElementById('title');
 var inputSlider = document.getElementById('input');
@@ -60,7 +60,6 @@ const upload = document.querySelector(".upload-button");
 const reset = document.querySelector(".reset-button");
 const file_input = document.querySelector('input[type=file]');
 const reader = new FileReader();
-
 uploadClick_area.addEventListener('click', function() {
     file_input.click();
 });
